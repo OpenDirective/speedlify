a591f256ad8362bef6e1e08bb7f7c102e6adee5b
Steve Lee <steve@opendirective.com>
2020-07-14 16:49:14 +0100

    Update and rename zachleat.js to sites.js

---
+++ _data/sites/sites.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: "sites", // optional, falls back to object key
+	description: "various sites",
+	options: {
+		frequency: 60 * 23, // 23 hours
+		freshChrome: "site"
+	},
+	urls: [
+		"https://opendirective.com/",
+		"https://opendirective.net/",
+		"https://musicpracticetools.net/",
+		"http://blog.fullmeasure.uk/",
+		"http://blog.fullmeasure.uk/"	
+	]
+};
